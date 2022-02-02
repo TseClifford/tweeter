97017bc5daba16f105876d0be8b29948af597bff
Clifford Tse <TseClifford@gmail.com>
2022-02-02 20:24:53 +0000

    add fetching tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,29 @@
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 // Append tweets to tweet-container from tweet array
 const renderTweets = (tweetsArr) => {
@@ -59,9 +59,15 @@ const createTweetElement = (tweet) => {
   return $tweet;
 };
 
-// Load render tweet when document ready
+// Get tweets then pass to renderTweets function
+const loadTweets = () => {
+  $.ajax("/tweets", { method: "GET" })
+    .then((data) => renderTweets(data));
+};
+
+// Load when document ready
 $(() => {
-  renderTweets(data);
+  loadTweets();
 
   $("form").on("submit", (event) => {
     event.preventDefault();
