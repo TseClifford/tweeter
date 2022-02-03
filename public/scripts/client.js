7f1ff947641123cf368f477db18b55edf06e02a2
Clifford Tse <TseClifford@gmail.com>
2022-02-03 23:31:33 +0000

    add responsive design, minor update to layout

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,3 @@
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
 const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -104,12 +77,14 @@ const toggleNewTweet = () => {
   });
 };
 
+// Hovering over element to toggle class changes
+$(".fa-angles-down").hover(() => $(this).toggleClass(".fa-angles-down:hover"));
+$("button").hover(() => $(this).toggleClass("button:hover"));
+$("article.tweet").hover(() => $(this).toggleClass("article.tweet:hover"));
+
 // Initial load when document ready
 $(() => {
   loadTweets();
   submitTweet();
   toggleNewTweet();
-  $(".fa-angles-down").hover(() => $(this).toggleClass(".fa-angles-down:hover"));
-  $("button").hover(() => $(this).toggleClass("button:hover"));
-  $("article.tweet").hover(() => $(this).toggleClass("article.tweet:hover"));
 });
